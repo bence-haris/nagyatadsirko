e23c6c34b7d1d8ec0ba8401ed2404a879221c982
BenHaris <gibson.bence@citromail.com>
2023-08-04 21:31:39 +0200

    Redesigned, About us added, animations added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+function checkForVisibility() {
+  var headers = document.querySelectorAll(".header");
+  headers.forEach(function(header) {
+    if (isElementInViewport(header)) {
+      header.classList.add("headerVisible");
+    } else {
+      header.classList.remove("headerVisible");
+    }
+  });
+}
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+if (window.addEventListener) {
+  addEventListener("DOMContentLoaded", checkForVisibility, false);
+  addEventListener("load", checkForVisibility, false);
+  addEventListener("scroll", checkForVisibility, false);
+}
 
 // Navigation: Compares nav-links to actual site: if a nav-link matches => this link is active
 $(document).ready(function(){
@@ -129,3 +157,4 @@ function checking(event){
     //Scrolls to page bottom
     window.scrollTo(0, document.body.scrollHeight);
 }
+
